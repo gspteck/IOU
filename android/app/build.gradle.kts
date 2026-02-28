0440f8bce03999b7768e1cc56a7766aff8d16a28
GSPTeck <66552124+gspteck@users.noreply.github.com>
2026-02-28 10:11:10 +0100

    small edits

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.gspteck.iou"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23 
+        minSdk = flutter.minSdkVersion 
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- lib/services/feedback.dart
+++ lib/services/feedback.dart
@@ -6,28 +6,27 @@ import 'package:path_provider/path_provider.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class FeedbackServices {
-	sendFeedback(context) async {
-		BetterFeedback.of(context).show((feedback) async {
-			// draft an email and send to developer
-			final screenshotFilePath =
-			await writeImageToStorage(feedback.screenshot);
+  sendFeedback(context) async {
+    BetterFeedback.of(context).show((feedback) async {
+      // draft an email and send to developer
+      final screenshotFilePath = await writeImageToStorage(feedback.screenshot);
 
-			final Email email = Email(
-				body: feedback.text,
-				subject: 'App Feedback',
-				recipients: ['gspteck@gmail.com'],
-				attachmentPaths: [screenshotFilePath],
-				isHTML: false,
-			);
-			await FlutterEmailSender.send(email);
-		});
-	}
+      final Email email = Email(
+        body: feedback.text,
+        subject: 'IOU - App Feedback',
+        recipients: ['gspteck@gmail.com'],
+        attachmentPaths: [screenshotFilePath],
+        isHTML: false,
+      );
+      await FlutterEmailSender.send(email);
+    });
+  }
 
-	Future<String> writeImageToStorage(Uint8List feedbackScreenshot) async {
-  	final Directory output = await getTemporaryDirectory();
-  	final String screenshotFilePath = '${output.path}/feedback.png';
-  	final File screenshotFile = File(screenshotFilePath);
-  	await screenshotFile.writeAsBytes(feedbackScreenshot);
-  	return screenshotFilePath;
-	}
+  Future<String> writeImageToStorage(Uint8List feedbackScreenshot) async {
+    final Directory output = await getTemporaryDirectory();
+    final String screenshotFilePath = '${output.path}/feedback.png';
+    final File screenshotFile = File(screenshotFilePath);
+    await screenshotFile.writeAsBytes(feedbackScreenshot);
+    return screenshotFilePath;
+  }
 }
